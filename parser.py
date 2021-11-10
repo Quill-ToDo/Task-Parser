c6ae24c68314c1c11d92f647350b99cf21971814
Lily Davisson <lilydavisson@gmail.com>
2021-11-10 17:45:42 -0500

    Make some progress, see new github issues

--- parser.py
+++ parser.py
@@ -9,11 +9,35 @@ def validate(input, output):
     '''
     Visualize differences between input and output dataset
     '''
-    pass
+
+    differences = []
+
+    for i in range(len(input)):
+        input_task = input[i]
+        output_task = output[i]
+        original = input_task["input"]
+        del input_task["input"]
+        if input_task != output_task:
+            input_task["input"] = original
+            differences.append({"input": input_task, "output": output_task})
+    
+    with open("differences.json", "w") as f:
+        json.dump(differences, f)
+    return len(differences) == 0
+
+def include_in_task(word):
+    return word.pos_ == "VERB" or word.pos_ == "ADJ" or word.pos_ == "AUX" or word.pos_ == "NOUN" or word.pos_ == "PROPN" or \
+            word.pos_ == "ADP"
+
 
 if __name__ == "__main__":
     # !!!Make sure you run this: $ python -m spacy download en_core_web_sm
-    
+
+    # TODO: 
+    # [ ] Read computer science as one group and not two separate things
+    # [ ] change input to include group in task
+    # [ ] Include auxillary words like do in task  
+    # [ ] Only include ADP in task if it is not before a date or in a date
     dataset = json.load(open(FILE))
 
     nlp = spacy.load("en_core_web_sm")
@@ -24,8 +48,7 @@ if __name__ == "__main__":
     for data in dataset:
         input_task = data["input"]
         doc = nlp(input_task)
-        answers = {}
-        answers["task"] = []
+        answers = { "group": None, "task": [], "date": None, "time": None }
 
         for word in doc:
             # check for acronyms before this because something like bio is recognized as an adjective
@@ -33,15 +56,16 @@ if __name__ == "__main__":
             if word.text in predefined_groups:
                 # Must be checked separately because these group names could be nouns, adjectives, etc.
                 answers["group"] = word.text
-                if word.pos_ == "VERB" or word.pos_ == "NOUN" or word.pos_ == "PROPN": 
-                    # Must come after date/time because dates are proper noun
-                    answers["task"].append(word.text)
+                # if include_in_task(word): 
+                #     # Must come after date/time because dates are proper noun
+                #     answers["task"].append(word.text)
 
             if word.ent_type_ == "DATE":
                 answers["date"] = word.text
-            elif word.ent_type == "TIME":
+            elif word.ent_type_ == "TIME":
                 answers["time"] = word.text
-            elif word.pos_ == "VERB" or word.pos_ == "NOUN" or word.pos_ == "PROPN": 
+            elif include_in_task(word): 
+                # THIS SHOULD NOT BE CHECKED TWICE BECAUSE THE WORD WILL BE ADDED TWICE
                 # Must come after date/time because dates are proper noun
                 answers["task"].append(word.text)
         
@@ -50,6 +74,9 @@ if __name__ == "__main__":
     
     with open("parsed_tasks.json", "w") as f:
         json.dump(results, f)
+
+    if not validate(dataset, results):
+        print("There were differences between the input and output files, check differences.json")
 
 
     # print(word.text, "pos:", word.pos_, "tag:", word.tag_, "ent type:", word.ent_type_)
